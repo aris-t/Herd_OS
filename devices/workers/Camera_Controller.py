16f10a059499fd848cecfa7022e5e2c8f113fa57
Christopher <swiftstar2@gmail.com>
2025-07-20 11:26:15 -0600

    fakesink

--- devices/workers/Camera_Controller.py
+++ devices/workers/Camera_Controller.py
@@ -45,7 +45,8 @@ class Camera_Controller(Worker):
                 "video/x-raw,width=640,height=480,framerate=30/1,format=I420",
                 "tee name=t",
                 "t. ! queue leaky=downstream max-size-buffers=2",
-                "shmsink socket-path=/tmp/video.shm shm-size=100000000 sync=false wait-for-connection=false"
+                "shmsink socket-path=/tmp/video.shm shm-size=100000000 sync=false wait-for-connection=false",
+                "t. ! fakesink"
             ]
 
         # Preview 
