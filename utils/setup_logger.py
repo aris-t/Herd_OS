117bd09511496c56dd06ea3a1ad54383d325c027
Christopher <swiftstar2@gmail.com>
2025-07-24 15:09:20 -0600

    updating logger

--- utils/setup_logger.py
+++ utils/setup_logger.py
@@ -8,17 +8,20 @@ def setup_logger(name: str, logger_path: str = './logs.txt') -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
+    # Create formatter for file handler
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
-    # Replace StreamHandler with RichHandler for rich formatting in the console
-    ch = RichHandler()
-    ch.setFormatter(formatter)
-
+    # File handler with traditional formatting
     fh = logging.FileHandler(logger_path)
     fh.setFormatter(formatter)
 
-    ch = logging.StreamHandler()
-    ch.setFormatter(formatter)
+    # Console handler with Rich formatting
+    ch = RichHandler(
+        show_time=True,
+        show_level=True,
+        show_path=True,
+        log_time_format="[%Y-%m-%d %H:%M:%S]"
+    )
 
     if not logger.handlers:
         logger.addHandler(fh)
