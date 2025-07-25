6c5d6afe8bf21b300e18e060d2248b138974c5d9
Christopher <swiftstar2@gmail.com>
2025-07-25 11:11:48 -0600

    ERA

--- launch.py
+++ launch.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     for device in devices:
         device.start()
 
-    time.sleep(2)  # Allow some time for devices to initialize
+    time.sleep(10)  # Allow some time for devices to initialize
     camera.put_command("start_recorder", None)
     time.sleep(5)  # Allow some time for the recorder to start
     camera.put_command("stop_recorder", None)
